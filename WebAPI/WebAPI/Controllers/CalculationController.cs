0c4f8a5463718e31cfb0d48aedda06242a1801da
edAgius <ed.agius@gmail.com>
2018-09-19 15:36:50 +0100

    add one

---
+++ WebAPI/WebAPI/Controllers/CalculationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class CalculationController : ApiController
+    {
+        [HttpGet]
+        [Route("api/1.0/Sum")]
+        ///rnd1={0}&rnd2={1}  
+        public int Sum(int rnd1, int rnd2)
+        {
+            return rnd1 + rnd2 + 1;
+        }
+
+    }
+}
